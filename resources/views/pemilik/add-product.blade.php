cda9df9712d8656a7aa6e90cdb32d89418a0692f
Ace <akhmadfauzy40@gmail.com>
2024-06-16 01:40:35 +0700

    Checkpoint

--- resources/views/pemilik/add-product.blade.php
+++ resources/views/pemilik/add-product.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid px-4">
         <h1 class="mt-4">Produk Bibit</h1>
         <ol class="breadcrumb mb-4">
-            <li class="breadcrumb-item"><a href="/pemilik/product-bibit">Produk Bibit</a></li>
+            <li class="breadcrumb-item"><a href="/pemilik/produk-bibit">Produk Bibit</a></li>
             <li class="breadcrumb-item active">Tambah Produk Bibit</li>
         </ol>
         <div class="card mb-4">
